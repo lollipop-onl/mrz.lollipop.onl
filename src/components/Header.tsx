a44203bbdfc837335d095b47405cf45265e18c64
simochee <lollipop@simochee.net>
2024-02-21 18:35:26 +0900

    🛀 cleanup repo

--- src/components/Header.tsx
+++
@@ -1,7 +0,0 @@
-export const Header: React.FC = () => {
-  return (
-    <header className="h-16 px-5 bg-slate-700 flex items-center">
-      <h1 className="text-slate-100 text-lg">Passport MRZ Simulator</h1>
-    </header>
-  );
-};
