326e3e73a7e75761ac4c01a19cc285024279865a
simochee <lollipop@simochee.net>
2024-02-21 15:46:18 +0900

    ✨ setup tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
+import "tailwindcss/tailwind.css";
 
 const root = document.getElementById("root")!;
 createRoot(root).render(<div>Hello World</div>);
