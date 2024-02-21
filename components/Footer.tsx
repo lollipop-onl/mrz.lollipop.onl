c0878a3e42cefe5e2d7e6920b26770a046afc37a
simochee <lollipop@simochee.net>
2024-02-21 19:32:58 +0900

    👕 run biome

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,11 @@
 export function Footer() {
-  return (
-    <footer className="py-6 px-5 bg-slate-500">
-      <div className="max-w-screen-lg mx-auto">
-        <p className="text-slate-100 text-sm">
-          &copy; 2021 Passport MRZ Simulator
-        </p>
-      </div>
-    </footer>
-  );
+	return (
+		<footer className="py-6 px-5 bg-slate-500">
+			<div className="max-w-screen-lg mx-auto">
+				<p className="text-slate-100 text-sm">
+					&copy; 2021 Passport MRZ Simulator
+				</p>
+			</div>
+		</footer>
+	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./{app,components}/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        body: ["IBM Plex Sans JP", "sans-serif"],
-      },
-    },
-  },
-  plugins: [],
+	content: ["./{app,components}/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				body: ["IBM Plex Sans JP", "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
 };
